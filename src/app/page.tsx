e524052adade72bacf9e80ec8dce3234987efbd0
Xinyu <Xinyu@Thes-MBP.lan>
2025-02-19 19:29:09 +0000

    added second screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/navbar";
 export default function Home() {
   return (
     <>
+    <img className="fixed right-3 bottom-3 size-20" src="chatbot.png" />
     <Navbar />
 
     {/* 1st screen */}
@@ -18,7 +19,17 @@ export default function Home() {
       </div>
     </div>
 
-    <img className="fixed right-3 bottom-3 size-20" src="chatbot.png" />
+    {/* 2nd screen */}
+    <div className="flex flex-col h-screen">
+      <div className="flex flex-1 justify-center">
+        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Issue Reporter</button>
+        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Contributor</button>
+      </div>
+      <div className="flex-1 m-10">
+        <img src="reporter-preview.png" />
+      </div>
+      
+    </div>
     </>
   );
 }
