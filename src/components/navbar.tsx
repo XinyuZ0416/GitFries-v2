addd10831c09767caa61182521145571a4e53cd7
Xinyu <Xinyu@Thes-MBP.lan>
2025-02-19 22:05:41 +0000

    added filter

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Filter from './filter'
 
 export default function Navbar() {
   return (
@@ -44,6 +45,8 @@ export default function Navbar() {
               </svg>
           </button>
         </div>
+
+        <Filter />
 
         <a href="/sign-up" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">Post an issue</a>
 
