28169d2ddc7f01c47ede1be07c8f162de99b6e1e
Xinyu <Xinyu@Thes-MBP.lan>
2025-02-26 18:51:03 +0000

    added beginner friendly and urgent checkbox

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -4,6 +4,22 @@ import React from 'react'
 export default function IssuesPage() {
   return (
     <>
+    <div className="flex">
+      <div>
+        <h2 className="text-2xl font-bold">Languages</h2>
+      </div>
+      <div className="flex ml-auto">
+        <div className="flex items-center me-4">
+          <input id="inline-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+          <label htmlFor="inline-checkbox" className="ms-2 text-m font-bold">Beginner Friendly</label>
+        </div>
+        <div className="flex items-center me-4">
+          <input id="inline-2-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+          <label htmlFor="inline-2-checkbox" className="ms-2 text-m font-bold">Urgent</label>
+        </div>
+      </div>
+    </div>
+    
     <LanguageCarousel />
     </>
   )
