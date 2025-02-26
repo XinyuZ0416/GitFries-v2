8cd9a5f8c4c037d58802dc7cf9249a18a73bed4a
Xinyu <Xinyu@Thes-MBP.lan>
2025-02-26 20:29:23 +0000

    add fav checkbox

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -10,6 +10,10 @@ export default function IssuesPage() {
         <h2 className="text-2xl font-bold">Languages</h2>
       </div>
       <div className="flex ml-auto">
+      <div className="flex items-center me-4">
+          <input id="inline-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+          <label htmlFor="inline-checkbox" className="ms-2 text-m font-bold">Favorited</label>
+        </div>
         <div className="flex items-center me-4">
           <input id="inline-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
           <label htmlFor="inline-checkbox" className="ms-2 text-m font-bold">Beginner Friendly</label>
