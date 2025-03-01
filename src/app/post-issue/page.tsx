109c4420c06d215c5db24e85cfb8b26885b6525d
Xinyu <Xinyu@Thes-MBP.lan>
2025-03-01 17:53:27 +0000

    removed headless ui in post issue

--- src/app/post-issue/page.tsx
+++ src/app/post-issue/page.tsx
@@ -1,30 +1,38 @@
+'use client'
 import RequireSignInSignUp from '@/components/require-signin-signup'
-import { Select, Textarea } from '@headlessui/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function PostIssuePage() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null; // Avoid rendering until mounted on client-side
+  
   return (
     <>
-    <RequireSignInSignUp target='Post an Issue' />
+    {/* <RequireSignInSignUp target='Post an Issue' /> */}
     {/* 如果用户已经登陆 */}
-    {/* <div className='flex justify-center items-center h-screen'>
+    <div className='flex justify-center items-center h-screen'>
       <form className="mx-auto w-2/5">
-        <div className="mb-5">
+        <fieldset className="mb-5">
           <label htmlFor="issue_url" className="block mb-2 text-sm font-medium">Issue URL *</label>
           <input type="text" id="issue_url" name='issue_url' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-        </div>
-        <div className="mb-5">
+        </fieldset>
+        <fieldset className="mb-5">
           <label htmlFor="issue_title" className="block mb-2 text-sm font-medium">Issue Title *</label>
           <input type="text" id="issue_title" name='issue_title' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-        </div>
-        <div className="mb-5">
+        </fieldset>
+        <fieldset className="mb-5">
           <label htmlFor="issue_description" className="block mb-2 text-sm font-medium">Description</label>
-          <Textarea id="issue_description" name='issue_description' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-        </div>
+          <textarea id="issue_description" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+        </fieldset>
         <div className="mb-5 flex flex-row">
-          <div>
+          <fieldset className="max-w-sm mx-auto">
             <label htmlFor="issue_language" className="block mb-2 text-sm font-medium">Language *</label>
-            <Select id="issue_language" name='issue_language' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+            <select id="issue_language" defaultValue={'C'} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <option value='C'>C</option>
               <option value='C++'>C++</option>
               <option value='C#'>C#</option>
@@ -47,23 +55,23 @@ export default function PostIssuePage() {
               <option value='Swift'>Swift</option>
               <option value='TypeScript'>TypeScript</option>
               <option value='Others'>Others</option>
-            </Select>
-          </div>
-          <div>
+            </select>
+          </fieldset>
+          <fieldset className="max-w-sm mx-auto">
             <label htmlFor="issue_difficulty" className="block mb-2 text-sm font-medium">Difficulty *</label>
-            <Select id="issue_difficulty" name='issue_difficulty' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-              <option value='beginner-friendly'>Beginner Friendly</option>
+            <select id="issue_difficulty" defaultValue={'headache'} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <option value='headache'>Headache</option>
-            </Select>
-          </div>
-          <div className="flex items-center">
+              <option value='beginner-friendly'>Beginner Friendly</option>
+            </select>
+          </fieldset>
+          <fieldset className="flex items-center">
             <input id="checkbox-1" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" ></input>
             <label htmlFor="checkbox-1" className="ms-2 text-sm font-medium">Urgent</label>
-          </div>
+          </fieldset>
         </div>
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
       </form>
-    </div> */}
+    </div>
     </>
   )
 }
--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -9,7 +9,7 @@ export default function Searchbar() {
           <label htmlFor="search-dropdown" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Filter</label>
           <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">Filter 
             <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
             </svg>
           </button>
 
