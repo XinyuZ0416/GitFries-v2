5e6f57ab3c807278200b54c2554e5c5cb84eead7
Xinyu <Xinyu@Thes-MBP.lan>
2025-03-01 16:44:32 +0000

    added forgot password

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function SignInPage() {
   return (
     <>
-    <div className='flex justify-center items-center h-screen'>
-      <form className="mx-auto w-2/5">
+    <div className='flex flex-col justify-center items-center h-screen'>
+      <form className="flex flex-col mx-auto w-2/5">
         <div className="mb-5">
           <label htmlFor="email" className="block mb-2 text-sm font-medium">Your email</label>
           <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gitfries.com" required />
@@ -19,6 +19,7 @@ export default function SignInPage() {
           </div>
           <label htmlFor="remember" className="ms-2 text-sm font-medium">Remember me because I'm cool</label>
         </div>
+        <a href='/forgot-password'>What was my password again?</a>
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign In</button>
       </form>
     </div>
