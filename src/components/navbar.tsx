fa21036963710b0412ace1ce0b6af9506e477d3c
Xinyu <Xinyu@Thes-MBP.lan>
2025-03-01 17:06:22 +0000

    added notifications page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -32,7 +32,9 @@ export default function Navbar() {
         {/* <a href="/sign-in" className="hover:underline">Sign In</a>
         <a href="/sign-up" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">Sign Up</a> */}
 
-        <img src="/notification.png" className="h-8" alt="no notification" />
+        <a href='/notifications'>
+          <img src="/notification.png" className="h-8" alt="no notification" />
+        </a>
         <img className="rounded-full h-8" src="/potato.png" alt="user profile" />
       </div>
     </nav>
