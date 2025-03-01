3fc31fe80b7c4b96c1d0303cd89c789573e81a18
Xinyu <Xinyu@Thes-MBP.lan>
2025-03-01 10:51:16 +0000

    modified sign in link

--- src/components/require-signin-signup.tsx
+++ src/components/require-signin-signup.tsx
@@ -8,7 +8,7 @@ export default function RequireSignInSignUp({target}: RequireSignInSignUpProps) 
   return (
     <>
     <div className="flex flex-col h-screen justify-center items-center">
-      <h1 className="text-4xl font-bold"><a href="/sign-up" className='underline'>Sign In</a> or <a href="/sign-up" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">Sign Up</a> to {target}</h1>
+      <h1 className="text-4xl font-bold"><a href="/sign-in" className='underline'>Sign In</a> or <a href="/sign-up" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">Sign Up</a> to {target}</h1>
     </div>
     </>
   )
