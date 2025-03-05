00892b86bf89de18bc99eed5cefec5d5bbe3e970
Xinyu <Xinyu@Thes-MBP.lan>
2025-03-05 21:54:56 +0000

    added basic sign in feature

--- src/components/sign-in-card.tsx
+++ src/components/sign-in-card.tsx
@@ -1,7 +1,8 @@
 'use client'
-import React, { useEffect, useState } from 'react';
-import { getAuth, createUserWithEmailAndPassword, sendSignInLinkToEmail, isSignInWithEmailLink, signInWithEmailLink, getAdditionalUserInfo } from "firebase/auth";
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '@/app/firebase';
+
 
 export default function SignInCard() {
   const [email, setEmail] = useState('');
@@ -9,73 +10,15 @@ export default function SignInCard() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    // Confirm the link is a sign-in with email link.
-    const auth = getAuth();
-    if (isSignInWithEmailLink(auth, window.location.href)) {
-      // Additional state parameters can also be passed via URL.
-      // This can be used to continue the user's intended action before triggering
-      // the sign-in operation.
-      // Get the email if available. This should be available if the user completes
-      // the flow on the same device where they started it.
-      let email = window.localStorage.getItem('emailForSignIn');
-      if (!email) {
-        // User opened the link on a different device. To prevent session fixation
-        // attacks, ask the user to provide the associated email again. For example:
-        email = window.prompt('Please provide your email for confirmation');
-      }
-      // The client SDK will parse the code from the link for you.
-      signInWithEmailLink(auth, email!, window.location.href)
-        .then((result) => {
-          // Clear email from storage.
-          window.localStorage.removeItem('emailForSignIn');
-          // You can access the new user by importing getAdditionalUserInfo
-          // and calling it with result:
-          console.log(getAdditionalUserInfo(result));
-          // You can access the user's profile via:
-          getAdditionalUserInfo(result)?.profile
-          // You can check if the user is new or existing:
-          getAdditionalUserInfo(result)?.isNewUser
-        })
-        .catch((error) => {
-          // Some error occurred, you can inspect the code: error.code
-          // Common errors could be invalid email and invalid or expired OTPs.
-          console.error('Error signing in with email link:', error.code);
-          // error.code:
-          // auth/missing-email
-          // auth/invalid-action-code
-          setError(error.message);
-          
-        });
-    }
-  }, []);
-
-  const actionCodeSettings = {
-    // URL you want to redirect back to. The domain (www.example.com) for this
-    // URL must be in the authorized domains list in the Firebase Console.
-    url: 'http://localhost:3000/sign-in',
-    // This must be true.
-    handleCodeInApp: true,
-    // The domain must be configured in Firebase Hosting and owned by the project.
-    // linkDomain: 'custom-domain.com'
-  };
-
   const handleSubmit = async(e) => {
     e.preventDefault();
 
-    setLoading(true);
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      await sendSignInLinkToEmail(auth, email, actionCodeSettings);
-      window.localStorage.setItem('emailForSignIn', email);
-      console.log('sent email!')
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      console.log(userCredential.user);
     } catch (error) {
-      setError(error.message);
-      console.error("Error signing up:", error.message);
       const errorCode = error.code;
-      const errorMessage = error.message;
-    } finally {
-      setLoading(false);
+      setError(error.message);
     }
   }
 
@@ -91,7 +34,7 @@ export default function SignInCard() {
         <label htmlFor="password" className="block mb-2 text-sm font-medium">Your password</label>
         <div className='flex flex-row relative'>
           <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
-            type={showPassword ? 'text' : 'password'} id="password" placeholder="DefinitelyNot123456" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            type='password' id="password" placeholder="DefinitelyNot123456" value={password} onChange={(e) => setPassword(e.target.value)} required />
         </div>
       </div>
       <div className="flex items-start mb-5">
--- src/components/sign-up-card.tsx
+++ src/components/sign-up-card.tsx
@@ -7,7 +7,6 @@ export default function SignUpCard() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
-  const [username, setUsername] = useState('');
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -115,13 +114,6 @@ export default function SignUpCard() {
           type="password" name="repeat_password" id="floating_repeat_password" placeholder=" " value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} required />
         <label htmlFor="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
           Confirm password {password == repeatPassword ? '': '(Fill in the Same Password as Above)'}
-        </label>
-      </div>
-      <div className="relative z-0 w-full mb-5 group">
-        <input className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-          type="text" name="floating_username" id="floating_username"  placeholder=" " value={username} onChange={(e) => setUsername(e.target.value)} required />
-        <label htmlFor="floating_username" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-          Username
         </label>
       </div>
       <div className="flex items-center mb-4">
