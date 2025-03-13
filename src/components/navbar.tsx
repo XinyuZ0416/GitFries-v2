50cf5dd2005b8b240f3c7262a89bbe0d0b78d90f
Xinyu <Xinyu@Thes-MBP.lan>
2025-03-13 21:10:43 +0000

    fixed user doc failed to create without refreshing

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -51,7 +51,7 @@ export default function Navbar() {
         { // if user is logged out or not verified
           !isVerified && 
           <>
-          <Link href="/sign-in" className="hover:underline">Sign In</Link>
+          <a href="/sign-in" className="hover:underline">Sign In</a>
           <Link href="/sign-up" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">Sign Up</Link>
           </>
         }
