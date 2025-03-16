63dea8587f953f42c1338a5258d029cd81652985
Xinyu <Xinyu@Thes-MBP.lan>
2025-03-16 17:58:53 +0000

    fixed settings page not showing username and bio first time sign in after sign up

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,10 +1,10 @@
 'use client'
-import { onAuthStateChanged, sendEmailVerification, sendPasswordResetEmail, updateEmail, verifyBeforeUpdateEmail } from 'firebase/auth';
+import { sendPasswordResetEmail, verifyBeforeUpdateEmail } from 'firebase/auth';
 import React, { useEffect, useState } from 'react'
 import { auth, db, storage } from '../firebase';
 import { useAuth } from '@/components/providers';
 import { useRouter } from 'next/navigation';
-import { doc, updateDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 type FormDataType = {
@@ -15,9 +15,11 @@ type FormDataType = {
 export default function Settings() {
   const [ isLoading, setIsLoading ] = useState<boolean>(false);
   const [ isResetPassword, setIsResetPassword ] = useState<boolean>(false);
+  const [ placeHolderUsername, setPlaceHolderUsername] = useState<string>('');
+  const [ placeHolderBio, setPlaceHolderBio] = useState<string>('');
   const [ newEmail, setNewEmail ] = useState<string>('');
   const [ errorCode, setErrorCode ] = useState<string>('');
-  const { email, username, bio, setUid, setIsVerified, userDbId, setUserPicUrl } = useAuth();
+  const { email, username, bio, uid, setUid, setIsVerified, userDbId, setUserPicUrl } = useAuth();
   const router = useRouter();
   const [ formData, setFormData ] = useState<FormDataType>({
     username: '',
@@ -26,6 +28,21 @@ export default function Settings() {
   const fileTypes = ["image/jpeg", "image/jpg", "image/png",];
 
   // TODO: if no user/ user not verified, dont show content
+
+  useEffect(() => {
+    const getUsernameAndBio = async() => {
+      const userDocRef = doc(db, "users", uid);
+      const userDocSnap = await getDoc(userDocRef);
+
+      if (userDocSnap.exists()) {
+        const userData = userDocSnap.data();
+        setPlaceHolderUsername(userData?.username || '');
+        setPlaceHolderBio(userData?.bio || '');
+      }
+    }
+    
+    getUsernameAndBio();
+  }, []);
 
   const handleUpdate = async() => {
     // Only update modified fields
@@ -148,12 +165,12 @@ export default function Settings() {
         <fieldset className="mb-5">
           <label htmlFor="username" className="block mb-2 text-sm font-medium">Username</label>
           <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            type="text" id="username" name='username' placeholder={username} value={formData.username} onChange={handleChange} />
+            type="text" id="username" name='username' placeholder={placeHolderUsername} value={formData.username} onChange={handleChange} />
         </fieldset>
         <fieldset className="mb-5">
           <label htmlFor="bio" className="block mb-2 text-sm font-medium">Bio</label>
           <textarea className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-            id="bio" name="bio" placeholder={bio} value={formData.bio} rows={4} onChange={handleChange} ></textarea>
+            id="bio" name="bio" placeholder={placeHolderBio} value={formData.bio} rows={4} onChange={handleChange} ></textarea>
         </fieldset>
         <button className="text-white bg-yellow-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           type='button' onClick={handleResetPassword}>
