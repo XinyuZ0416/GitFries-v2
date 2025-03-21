f68d9d6a26d6d2c15c73373d6b02ad6b18dfba4a
Xinyu <Xinyu@Thes-MBP.lan>
2025-03-21 17:45:58 +0000

    removed preview in md and added height for preview card

--- src/components/issue-preview.tsx
+++ src/components/issue-preview.tsx
@@ -1,8 +1,6 @@
 import formatDate from '@/utils/format-date';
-import MDEditor from '@uiw/react-md-editor';
 import { Timestamp } from 'firebase/firestore'
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import React from 'react'
 
 interface PreviewCardProps {
@@ -23,14 +21,13 @@ export default function PreviewCard({
 } : PreviewCardProps) {
 
   const formattedDate = formatDate(time.toDate());
-  const router = useRouter();
 
   return (
     <>
-    <div className={`flex flex-row p-3 items-center rounded-lg shadow-lg hover:bg-gray-100
+    <Link className={`flex flex-row p-3 h-32 overflow-hidden items-center rounded-lg shadow-lg hover:bg-gray-100
       ${isUrgent ? `shadow-red-500` : `shadow-grey`}
       ${difficulty === `beginner-friendly` ? `bg-green-200` : `bg-white`}`}
-      onClick={() => router.push(`/issues/${issueId}`)}>
+      href={`/issues/${issueId}`} >
       <div>
         <img className="rounded-full size-14" src={issueReporterPicUrl} alt="user profile" />
         <h6 className='text-lg font-bold'>{issueReporterUsername}</h6>
@@ -38,9 +35,7 @@ export default function PreviewCard({
 
       <div className="flex flex-col justify-between px-4 py-2">
         <h5 className="text-xl font-bold">{title}</h5>
-        <section id='issue-description' className='my-3' data-color-mode="light">
-          <MDEditor.Markdown source={description} />
-        </section>
+        <p className="font-normal text-gray-700">{description}</p>
       </div>
 
       <div>
@@ -48,7 +43,7 @@ export default function PreviewCard({
         <p className="font-normal text-gray-700">{language}</p>
         <p className="font-normal text-gray-700">{difficulty}</p>
       </div>
-    </div>
+    </Link>
     </>
   )
 }
