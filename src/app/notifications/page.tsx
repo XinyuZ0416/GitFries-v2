b51025e9a6eb0473f276d33f54468ce1fc9cb597
Xinyu <Xinyu@Thes-MBP.lan>
2025-03-23 19:03:04 +0000

    delete stuff on notification

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -2,21 +2,12 @@
 import NotificationsCommentCard from '@/components/notifications-comment-card'
 import NotificationsReplyCard from '@/components/notifications-reply-card'
 import { useAuthProvider } from '@/providers/auth-provider'
-import { doc, getDoc } from 'firebase/firestore'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
 import React, { useEffect } from 'react'
 import { db } from '../firebase'
 import { useCurrentUserDocProvider } from '@/providers/current-user-doc-provider'
 
 export default function NotificatonsPage() {
-  const { userDocData } = useCurrentUserDocProvider();
-
-  useEffect(() => {
-    const getUnreadNotif = () => {
-      console.log(userDocData)
-    }
-
-    getUnreadNotif();
-  }, [userDocData]);
 
   return (
     <>
