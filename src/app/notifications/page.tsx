33747ddd921f8c6a8b12763748653f2fd88d7e97
Xinyu <Xinyu@Thes-MBP.lan>
2025-03-24 19:42:12 +0000

    move unread notif to read

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -2,31 +2,38 @@
 import NotificationsCommentCard from '@/components/notifications-comment-card'
 import NotificationsReplyCard from '@/components/notifications-reply-card'
 import { useAuthProvider } from '@/providers/auth-provider'
-import { doc, getDoc, updateDoc } from 'firebase/firestore'
-import React, { useEffect } from 'react'
+import { arrayUnion, doc, getDoc, updateDoc } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
 import { db } from '../firebase'
 import { useCurrentUserDocProvider } from '@/providers/current-user-doc-provider'
 
 export default function NotificatonsPage() {
+  const [ readNotif, setReadNotif ] = useState<string[]>([])
   const { uid } = useAuthProvider();
   const{ unreadNotif, setUnreadNotif } = useCurrentUserDocProvider();
 
-  // Clean up unread notifications
+  // Move unread notifications to read
   useEffect(() => {
     if (unreadNotif.length > 0) {
-      const cleanUpUnreadNotif = async() => {
-        const ref = doc(db, "users", uid);
-        await updateDoc(ref, {unreadNotif:[]});
+      const moveNotif = async() => {
+        // Add to read
+        for (let notif of unreadNotif) {
+          await updateDoc(doc(db, "users", uid), {readNotif:arrayUnion(notif)});
+          setReadNotif(prev => [...prev, notif]);
+        }
 
+        // Remove from unread
+        await updateDoc(doc(db, "users", uid), {unreadNotif:[]});
         setUnreadNotif([]);
       }
-      cleanUpUnreadNotif();
+      moveNotif();
     }
   }, []);
 
   return (
     <>
     <div className='flex flex-col gap-2'>
+      Your notifications will be automatically deleted after 1 month
       <NotificationsCommentCard />
       <NotificationsReplyCard />
     </div>
