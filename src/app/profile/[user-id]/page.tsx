01702309af51b32bef9129000f3f76b37b0b457f
Xinyu <Xinyu@Thes-MBP.lan>
2025-04-08 19:46:14 +0100

    move ProfilePicCard data fetch intp profile page and render based on user-id

--- src/app/profile/[user-id]/page.tsx
+++ src/app/profile/[user-id]/page.tsx
@@ -3,17 +3,55 @@ import ProfileAchievementsCard from '@/components/profile/achievements'
 import ProfileActivities from '@/components/profile/activities/activities'
 import ProfileDashboardCard from '@/components/profile/dashboard'
 import ProfilePicCard from '@/components/profile/bio'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'next/navigation'
+import { useAuthProvider } from '@/providers/auth-provider'
+import { doc, getDoc } from 'firebase/firestore'
+import { db, storage } from '@/app/firebase'
+import { getDownloadURL, ref } from 'firebase/storage'
 
 export default function ProfilePage() {
-  
+  const { "user-id": userIdParam } = useParams();
+  const userId = Array.isArray(userIdParam) ? userIdParam[0] : userIdParam; // Ensure only string 
+  const { username, bio, uid, userPicUrl } = useAuthProvider();
+  const [ displayUsername, setDisplayUsername ] = useState<string>("");
+  const [ displayBio, setDisplayBio ] = useState<string>("");
+  const [ displayUserPicUrl, setDisplayUserPicUrl ] = useState<string>("");
+
+  const getUserInfo = async() => {
+    if (userId === uid) {
+      setDisplayUsername(username);
+      setDisplayBio(bio);
+      setDisplayUserPicUrl(userPicUrl);
+    } else {
+      const userDocSnap = await getDoc(doc(db, "users", userId!));
+      const userData = userDocSnap.data();
+      if (userData) {
+        setDisplayUsername(userData.username);
+        setDisplayBio(userData.bio);
+        try {
+          const url = await getDownloadURL(ref(storage, `user-img/${userId}`));
+          setDisplayUserPicUrl(url);
+        } catch {
+          setDisplayUserPicUrl("/potato.png");
+        }
+      }
+    }
+  }
   // TODO: if no user/ user not verified, dont show content
+  useEffect(() => {
+    getUserInfo();
+  }, [displayUsername, displayBio, displayUserPicUrl]);
   return (
     <>
     <div className='flex flex-col gap-2'>
       <div className='flex flex-row gap-2 w-full'>
         <div className='flex flex-col gap-2 w-fit justify-between'>
-          <ProfilePicCard />
+          <ProfilePicCard 
+            username={displayUsername}
+            bio={displayBio}
+            userPicUrl={displayUserPicUrl}
+          />
           <ProfileAchievementsCard />
         </div>
         <ProfileDashboardCard />
--- src/components/profile/bio.tsx
+++ src/components/profile/bio.tsx
@@ -1,13 +1,20 @@
 'use client'
-import { useAuthProvider } from '@/providers/auth-provider'
 import React from 'react'
 
-export default function ProfilePicCard() {
-  const { username, bio } = useAuthProvider();
+interface ProfilePicCardProps {
+  username: string,
+  bio: string,
+  userPicUrl: string,
+}
+
+export default function ProfilePicCard({
+  username,
+  bio,
+  userPicUrl}: ProfilePicCardProps) {
   return (
     <>
     <div className='flex flex-col flex-grow w-full max-w-md justify-center items-center rounded-lg shadow-sm p-4 bg-white'>
-      <img className='rounded-full size-14' src='/potato.png' alt='user profile picture' />
+      <img className='rounded-full size-14' src={userPicUrl} alt='user profile picture' />
       <h2 className='text-2xl font-bold'>{username}</h2>
       <p className='font-normal'>{bio}</p>
     </div>
