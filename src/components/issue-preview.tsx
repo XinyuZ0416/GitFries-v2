0610e02c1e224bc7d2e79013868b9f77016e32eb
Xinyu <Xinyu@Thes-MBP.lan>
2025-04-19 10:05:21 +0100

    modified the logic of issue preview card text color render to hide issue availability from signed-out users

--- src/components/issue-preview.tsx
+++ src/components/issue-preview.tsx
@@ -20,13 +20,18 @@ export default function PreviewCard({
   issueId, description, difficulty, isUrgent, 
   language, time, title, claimedBy, finishedBy
 } : PreviewCardProps) {
-  const [ isUnavailable, setIsUnavailable ] = useState<boolean>(false);
+  const [ isAvailable, setIsAvailable ] = useState<boolean>(true);
   const { uid } = useAuthProvider();
   const formattedDate = formatDate(time.toDate());
 
   useEffect(() => {
-    if (uid && claimedBy != null || finishedBy != null) {
-      setIsUnavailable(true);
+    // If user signed out, show all issues as available
+    if (!uid) {
+      setIsAvailable(true); 
+    } else {
+      if (claimedBy != null || finishedBy != null) {
+        setIsAvailable(false);
+      }
     }
   }, [uid, claimedBy]);
 
@@ -37,14 +42,14 @@ export default function PreviewCard({
       ${difficulty === `easy-fix` ? `bg-green-200` : `bg-white`}`}
       href={`/issues/${issueId}`} >
       <div className="flex flex-col justify-between px-4 py-2">
-        <h5 className={`text-xl font-bold ${isUnavailable && `text-gray-300`}`}>{title}</h5>
-        <p className={`font-normal ${isUnavailable ? `text-gray-300` : `text-gray-700`}`}>{description}</p>
+        <h5 className={`text-xl font-bold ${!isAvailable && `text-gray-300`}`}>{title}</h5>
+        <p className={`font-normal ${isAvailable ? `text-gray-700` : `text-gray-300`}`}>{description}</p>
       </div>
 
       <div>
-        <p className={`font-normal ${isUnavailable ? `text-gray-300` : `text-gray-700`}`}>{formattedDate}</p>
-        <p className={`font-normal ${isUnavailable ? `text-gray-300` : `text-gray-700`}`}>{language}</p>
-        <p className={`font-normal ${isUnavailable ? `text-gray-300` : `text-gray-700`}`}>{difficulty}</p>
+        <p className={`font-normal ${isAvailable ? `text-gray-700` : `text-gray-300`}`}>{formattedDate}</p>
+        <p className={`font-normal ${isAvailable ? `text-gray-700` : `text-gray-300`}`}>{language}</p>
+        <p className={`font-normal ${isAvailable ? `text-gray-700` : `text-gray-300`}`}>{difficulty}</p>
       </div>
     </Link>
     </>
