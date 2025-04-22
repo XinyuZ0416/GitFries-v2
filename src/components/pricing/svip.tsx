e553f75e75545d6ce61babc346713e694b172e01
Xinyu <Xinyu@Thes-MBP.lan>
2025-04-22 18:20:24 +0100

    change dollar to pounds

--- src/components/pricing/svip.tsx
+++ src/components/pricing/svip.tsx
@@ -6,7 +6,7 @@ export default function PricingSvipCard() {
     <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-8 dark:bg-gray-800 dark:border-gray-700">
       <h5 className="mb-4 text-xl font-medium text-gray-500">SVIP Plan</h5>
       <div className="flex items-baseline text-gray-900 dark:text-white">
-        <span className="text-3xl font-semibold">$</span>
+        <span className="text-3xl font-semibold">￡</span>
         <span className="text-5xl font-extrabold tracking-tight">19.99</span>
         <span className="ms-1 text-xl font-normal text-gray-500">/month</span>
       </div>
@@ -54,7 +54,7 @@ export default function PricingSvipCard() {
           <span className="text-base font-normal leading-tight text-gray-500 ms-3">SVIP Membership Icon</span>
         </li>
       </ul>
-      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Choose plan</button>
+      <button type="button" disabled className="text-white bg-gray-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Choose Plan (Coming Soon)</button>
     </div>
     </>
   )
