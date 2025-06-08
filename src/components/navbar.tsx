0ffef35e2b296a10f01dd384a4a3c5f9f20475f0
Xinyu <syuxxxsp@163.com>
2025-06-08 17:35:28 +0100

    modified post issue button on navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -65,9 +65,12 @@ export default function Navbar() {
           
           {/* Post issue button (default visible on desktop) */}
           <Link href="/post-issue" className="px-5 py-2.5 hidden md:inline-block">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-            </svg>
+            <div className='flex items-center'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5 mr-2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
+              Post Issue
+            </div>
           </Link>
           
           {/* Notifications, chats and user dropdown (default visible on desktop) */}
