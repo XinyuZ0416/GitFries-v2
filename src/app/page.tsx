2ab6c227188ae4e9eebd1fdfdbc074cb73c792e3
Xinyu <syuxxxsp@163.com>
2025-07-26 18:12:31 +0100

    added motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,23 @@
 import EmailSignUp from "@/components/email-signup";
 import { useAuthProvider } from "@/providers/auth-provider";
 import { useNavbarProvider } from "@/providers/navbar-provider";
+import { motion } from "framer-motion";
 
 export default function Home() {
   const { isVerified } = useAuthProvider();
   const { height } = useNavbarProvider();
   const navbarHeight = height ?? 64;
+  
+  const fadeInUp = {
+    hidden: { opacity: 0, y: 50},
+    visible: { opacity: 1, y: 0, transition: { duration: 0.6}},
+  }
 
   return (
     <>
     {/* 1st screen */}
-    <div className="flex mx-2" style={{ height: `calc(100vh - ${navbarHeight}px)` }}>
+    <motion.div className="flex mx-2" style={{ height: `calc(100vh - ${navbarHeight}px)` }}
+      initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
       <div className="flex flex-col flex-1 justify-center items-center">
         <h1 className="text-4xl font-bold text-center">Link Code Issues with Contributors</h1>
         { isVerified !== null && !isVerified  && <EmailSignUp goal="Find Contributors or Contribute to Issues" /> }
@@ -20,28 +27,31 @@ export default function Home() {
       <div className="flex flex-col flex-1 justify-center items-center">
         <img src="logo.png" />
       </div>
-    </div>
+    </motion.div>
 
     {/* 2nd screen */}
-    <div className="flex flex-col mx-4 mb-5 text-center">
+    <motion.div className="flex flex-col mx-4 mb-5 text-center"
+      initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
       <h1 className="text-4xl font-bold">Find Talented Contributors</h1>
       <h2 className="text-2xl font-semi-bold">Scratching your head for a bug? Post the issue on GitFries and find contributors.</h2>
-    </div>
-    <img src="2nd.png" className="max-w-full h-auto object-contain" />
+      <img src="2nd.png" className="max-w-full h-auto object-contain" />
+    </motion.div>
 
     {/* 3rd screen */}
-    <div className="flex flex-col mx-4 my-5 text-center">
+    <motion.div className="flex flex-col mx-4 my-5 text-center"
+      initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
       <h1 className="text-4xl font-bold">Showcase Skills by Contributing</h1>
       <h2 className="text-2xl font-semi-bold">Want to take part in more real-life projects? Fix issues on GitFries and track your growth.</h2>
-    </div>
-    <img src="3rd.png" className="max-w-full h-auto object-contain" />
+      <img src="3rd.png" className="max-w-full h-auto object-contain" />
+    </motion.div>
 
     {/* 4th screen */}
     { isVerified !== null && !isVerified && 
-    <div className="flex flex-col mx-4 justify-center items-center" style={{ height: `calc(100vh - ${navbarHeight}px)` }}>
+    <motion.div className="flex flex-col mx-4 justify-center items-center" style={{ height: `calc(100vh - ${navbarHeight}px)` }}
+      initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
       <h1 className="text-4xl font-bold">Join GitFries Today</h1>
       <EmailSignUp goal="Find Contributors or Contribute to Issues" />
-    </div>
+    </motion.div>
     }
     </>
   );
