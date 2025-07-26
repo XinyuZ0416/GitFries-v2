9021b8b2e58cf6a1077146813c44a33767c1d8a9
Xinyu <syuxxxsp@163.com>
2025-07-26 18:15:59 +0100

    slight ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,17 +32,17 @@ export default function Home() {
     {/* 2nd screen */}
     <motion.div className="flex flex-col mx-4 mb-5 text-center"
       initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
-      <h1 className="text-4xl font-bold">Find Talented Contributors</h1>
-      <h2 className="text-2xl font-semi-bold">Scratching your head for a bug? Post the issue on GitFries and find contributors.</h2>
-      <img src="2nd.png" className="max-w-full h-auto object-contain" />
+      <h1 className="text-4xl font-bold mb-4">Find Talented Contributors</h1>
+      <h2 className="text-2xl font-semi-bold mb-6">Scratching your head for a bug? Post the issue on GitFries and find contributors.</h2>
+      <img src="2nd.png" className="max-w-full h-auto object-contain rounded-xl shadow-md" />
     </motion.div>
 
     {/* 3rd screen */}
     <motion.div className="flex flex-col mx-4 my-5 text-center"
       initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
-      <h1 className="text-4xl font-bold">Showcase Skills by Contributing</h1>
-      <h2 className="text-2xl font-semi-bold">Want to take part in more real-life projects? Fix issues on GitFries and track your growth.</h2>
-      <img src="3rd.png" className="max-w-full h-auto object-contain" />
+      <h1 className="text-4xl font-bold mb-4">Showcase Skills by Contributing</h1>
+      <h2 className="text-2xl font-semi-bold mb-6">Want to take part in more real-life projects? Fix issues on GitFries and track your growth.</h2>
+      <img src="3rd.png" className="max-w-full h-auto object-contain rounded-xl shadow-md" />
     </motion.div>
 
     {/* 4th screen */}
