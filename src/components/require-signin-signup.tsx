e96194f7f9ae29d215e322f4f49e02f3851db5d5
Xinyu <syuxxxsp@163.com>
2025-07-27 17:52:40 +0100

    modify require signin signup

--- src/components/require-signin-signup.tsx
+++ src/components/require-signin-signup.tsx
@@ -1,3 +1,4 @@
+import { useNavbarProvider } from '@/providers/navbar-provider';
 import Link from 'next/link'
 import React from 'react'
 
@@ -6,10 +7,22 @@ interface RequireSignInSignUpProps {
 }
 
 export default function RequireSignInSignUp({target}: RequireSignInSignUpProps) {
+  const { height } = useNavbarProvider();
+  const navbarHeight = height ?? 64;
+    
   return (
     <>
-    <div className="flex flex-col h-screen justify-center items-center">
-      <h1 className="text-4xl font-bold"><Link href="/sign-in" className='underline'>Sign In</Link> or <Link href="/sign-up" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">Sign Up</Link> to {target}</h1>
+    <div style={{ height: `calc(100vh - ${navbarHeight}px)` }} className="flex flex-col h-screen justify-center items-center">
+      <h1 className="text-4xl font-bold text-center">
+        <span className="inline-block align-middle">
+          <Link href="/sign-in" className="underline">Sign In</Link>
+        </span> or 
+        <span className="inline-flex align-middle ml-2">
+          <Link href="/sign-up" className="transition-transform duration-150 hover:scale-105 border-4 border-black shadow-[4px_4px_0px_0px_black] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 focus:outline-none">
+            Sign Up
+          </Link>
+        </span> to {target}
+      </h1>
     </div>
     </>
   )
