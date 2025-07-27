5c819a355d3c1386d99bce300f78821f694a2cc4
Xinyu <syuxxxsp@163.com>
2025-07-27 10:38:11 +0100

    changed pics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,23 +38,23 @@ export default function Home() {
     </motion.div>
     
 
-    {/* 2nd screen */}
+    {/* 3rd screen */}
     <motion.div className="flex flex-col mx-4 my-24 text-center"
       initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
       <h1 className="text-4xl font-bold mb-4">Find Talented Contributors</h1>
       <h2 className="text-2xl font-semi-bold mb-6">Scratching your head for a bug? Post the issue on GitFries and find contributors.</h2>
-      <img src="2nd.png" className="max-w-full h-auto object-contain rounded-xl shadow-md" />
-    </motion.div>
-
-    {/* 3rd screen */}
-    <motion.div className="flex flex-col mx-4 my-24 text-center"
-      initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
-      <h1 className="text-4xl font-bold mb-4">Showcase Skills by Contributing</h1>
-      <h2 className="text-2xl font-semi-bold mb-6">Want to take part in more real-life projects? Fix issues on GitFries and track your growth.</h2>
       <img src="3rd.png" className="max-w-full h-auto object-contain rounded-xl shadow-md" />
     </motion.div>
 
     {/* 4th screen */}
+    <motion.div className="flex flex-col mx-4 my-24 text-center"
+      initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
+      <h1 className="text-4xl font-bold mb-4">Showcase Skills by Contributing</h1>
+      <h2 className="text-2xl font-semi-bold mb-6">Want to take part in more real-life projects? Fix issues on GitFries and track your growth.</h2>
+      <img src="4th.png" className="max-w-full h-auto object-contain rounded-xl shadow-md" />
+    </motion.div>
+
+    {/* 5th screen */}
     { isVerified !== null && !isVerified && 
     <motion.div className="flex flex-col mx-4 justify-center items-center" style={{ height: `calc(100vh - ${navbarHeight}px)` }}
       initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
