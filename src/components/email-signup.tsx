5c52350ddf5d652fd6f070424c1d4cb814f21e62
Xinyu <syuxxxsp@163.com>
2025-07-27 15:13:09 +0100

    make email sign up comic

--- src/components/email-signup.tsx
+++ src/components/email-signup.tsx
@@ -15,9 +15,11 @@ export default function EmailSignUp({goal}: EmailSignUpProps) {
     <>
       <form className="flex flex-col items-center w-full">
         <label className="text-3xl font-semibold text-center" htmlFor="email">Join GitFries to {goal}</label>
-        <div className='flex'>
-          <input type="email" id="email" placeholder="Enter your email" className="bg-white border border-gray-300 text-gray-900 md:w-64 mb-2 md:mb-0 md:me-4 text-lg rounded-lg focus:ring-orange-900 focus:border-orange-900 block w-full p-2.5" required />
-          <button onClick={handleSignUp} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center"> Sign Up - It's Free!</button>
+        <div className='flex mt-10'>
+          <input type="email" id="email" placeholder="Enter your email" 
+            className="shadow-[4px_4px_0px_0px_black] bg-white border-4 border-black transition-transform duration-150 hover:scale-105 text-gray-900 md:w-64 mb-2 md:mb-0 md:me-4 text-lg rounded-lg focus:ring-orange-900 focus:border-orange-900 block w-full p-2.5" required />
+          <button onClick={handleSignUp} 
+            className="shadow-[4px_4px_0px_0px_black] border-4 border-black transition-transform duration-150 hover:scale-105 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center"> Sign Up - It's Free!</button>
         </div>
       </form>
     </>
