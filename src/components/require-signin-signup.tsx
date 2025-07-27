15e713fff9bc16ccfb869766ce1e6973536755d7
Xinyu <syuxxxsp@163.com>
2025-07-27 17:08:02 +0100

    replace a with link on require sign in and sign up

--- src/components/require-signin-signup.tsx
+++ src/components/require-signin-signup.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 interface RequireSignInSignUpProps {
@@ -8,7 +9,7 @@ export default function RequireSignInSignUp({target}: RequireSignInSignUpProps) 
   return (
     <>
     <div className="flex flex-col h-screen justify-center items-center">
-      <h1 className="text-4xl font-bold"><a href="/sign-in" className='underline'>Sign In</a> or <a href="/sign-up" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">Sign Up</a> to {target}</h1>
+      <h1 className="text-4xl font-bold"><Link href="/sign-in" className='underline'>Sign In</Link> or <Link href="/sign-up" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">Sign Up</Link> to {target}</h1>
     </div>
     </>
   )
