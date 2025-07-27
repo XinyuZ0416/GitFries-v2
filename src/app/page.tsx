ced31f9ada9611c77501ff268b75fce190c986f1
Xinyu <syuxxxsp@163.com>
2025-07-27 11:02:11 +0100

    change font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
     <motion.div className="flex mx-2" style={{ height: `calc(100vh - ${navbarHeight}px)` }}
       initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
       <div className="flex flex-col flex-1 justify-center items-center">
-        <h1 className="text-4xl font-bold text-center">Link Code Issues with Contributors</h1>
+        <h1 className="text-7xl font-bold text-center">Link Code Issues with Contributors</h1>
         { isVerified !== null && !isVerified  && <EmailSignUp goal="Outsource or Contribute to Code Repo Issues" /> }
       </div>
 
@@ -42,8 +42,8 @@ export default function Home() {
     <motion.div className="grid grid-flow-row grid-cols-2 mx-4 my-24 text-center"
       initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
       <div className="flex flex-col flex-1 justify-center items-center mx-2">
-        <h1 className="text-4xl font-bold mb-4">Find Talented Contributors</h1>
-        <h2 className="text-2xl font-semi-bold mb-6">Scratching your head for a bug? Post the issue on GitFries and find contributors.</h2>
+        <h1 className="text-7xl font-bold mb-4">Find Talented Contributors</h1>
+        <h2 className="text-4xl font-semi-bold mb-6">Scratching your head for a bug? Post the issue on GitFries and find contributors.</h2>
       </div>
       <div>
         <img src="3rd.png" className="max-w-full h-auto object-contain rounded-xl shadow-md" />
@@ -57,8 +57,8 @@ export default function Home() {
         <img src="4th.png" className="max-w-full h-auto object-contain rounded-xl shadow-md" />
       </div>
       <div className="flex flex-col flex-1 justify-center items-center mx-2">
-        <h1 className="text-4xl font-bold mb-4">Showcase Skills by Contributing</h1>
-        <h2 className="text-2xl font-semi-bold mb-6">Want to take part in more real-life projects? Fix issues on GitFries and track your growth.</h2>
+        <h1 className="text-7xl font-bold mb-4">Showcase Skills by Contributing</h1>
+        <h2 className="text-4xl font-semi-bold mb-6">Want to take part in more real-life projects? Fix issues on GitFries and track your growth.</h2>
       </div>
     </motion.div>
 
@@ -66,7 +66,7 @@ export default function Home() {
     { isVerified !== null && !isVerified && 
     <motion.div className="flex flex-col mx-4 justify-center items-center" style={{ height: `calc(100vh - ${navbarHeight}px)` }}
       initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
-      <h1 className="text-4xl font-bold">Join GitFries Today</h1>
+      <h1 className="text-7xl font-bold">Join GitFries Today</h1>
       <EmailSignUp goal="Outsource or Contribute to Code Repo Issues" />
     </motion.div>
     }
