12024b8a3496a54bc8f1c6537f081e830679180e
Xinyu <syuxxxsp@163.com>
2025-07-27 10:47:16 +0100

    changed homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,19 +39,27 @@ export default function Home() {
     
 
     {/* 3rd screen */}
-    <motion.div className="flex flex-col mx-4 my-24 text-center"
+    <motion.div className="grid grid-flow-row grid-cols-2 mx-4 my-24 text-center"
       initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
-      <h1 className="text-4xl font-bold mb-4">Find Talented Contributors</h1>
-      <h2 className="text-2xl font-semi-bold mb-6">Scratching your head for a bug? Post the issue on GitFries and find contributors.</h2>
-      <img src="3rd.png" className="max-w-full h-auto object-contain rounded-xl shadow-md" />
+      <div className="flex flex-col flex-1 justify-center items-center mx-2">
+        <h1 className="text-4xl font-bold mb-4">Find Talented Contributors</h1>
+        <h2 className="text-2xl font-semi-bold mb-6">Scratching your head for a bug? Post the issue on GitFries and find contributors.</h2>
+      </div>
+      <div>
+        <img src="3rd.png" className="max-w-full h-auto object-contain rounded-xl shadow-md" />
+      </div>
     </motion.div>
 
     {/* 4th screen */}
-    <motion.div className="flex flex-col mx-4 my-24 text-center"
+    <motion.div className="grid grid-flow-row grid-cols-2 mx-4 my-24 text-center"
       initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3}} variants={fadeInUp}>
-      <h1 className="text-4xl font-bold mb-4">Showcase Skills by Contributing</h1>
-      <h2 className="text-2xl font-semi-bold mb-6">Want to take part in more real-life projects? Fix issues on GitFries and track your growth.</h2>
-      <img src="4th.png" className="max-w-full h-auto object-contain rounded-xl shadow-md" />
+      <div>
+        <img src="4th.png" className="max-w-full h-auto object-contain rounded-xl shadow-md" />
+      </div>
+      <div className="flex flex-col flex-1 justify-center items-center mx-2">
+        <h1 className="text-4xl font-bold mb-4">Showcase Skills by Contributing</h1>
+        <h2 className="text-2xl font-semi-bold mb-6">Want to take part in more real-life projects? Fix issues on GitFries and track your growth.</h2>
+      </div>
     </motion.div>
 
     {/* 5th screen */}
